98481ff2f9d33f7a81be9ddb0ff2eb04d197ba74
jdmarmen <jdmarmen@etsinf.upv.es>
2024-11-19 19:17:49 +0100

    Added Dict.h file

---
+++ Dict.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+template <typename V>
+class Dict
+{
+	public:
+		virtual void insert(std::string key, V value)=0;
+		virtual V search(std::string key)=0;
+		virtual V remove(std:: string key)=0;
+		virtual int entries()=0;
+
+};
